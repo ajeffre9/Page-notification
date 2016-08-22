a489468155a544a36f6fc8f2f956d73eb6c0ff28
ajeffre9 <jeffrey.anwar1@gmail.com>
2016-08-22 17:06:11 -0400

    first try

--- Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
+++ Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.basicnotifications;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -47,13 +48,13 @@ public class MainActivity extends Activity {
         // Create builder for the main notification
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.ic_alarm_black_18dp)
-                        .setContentTitle("Page 1")
-                        .setContentText("Short message")
-                        .setContentIntent(intent);
+                        .setSmallIcon(R.drawable.ic_whatshot_black_18dp)
+                        .setContentTitle("Daily tip")
+                        .setContentText("Insert daily tip here")
+                        .setContentIntent(pendingIntent);
 
         // Create a big text style for the second page
-        BigTextStyle secondPageStyle = new NotificationCompat.BigTextStyle();
+        NotificationCompat.BigTextStyle secondPageStyle = new NotificationCompat.BigTextStyle();
         secondPageStyle.setBigContentTitle("Page 2")
                 .bigText("A lot of text...");
 
@@ -69,65 +70,10 @@ public class MainActivity extends Activity {
                         .addPage(secondPageNotification))
                 .build();
 
-        // Issue the notification
-        notificationManager =
-                NotificationManagerCompat.from(this);
-        notificationManager.notify(notificationId, notification);
 
-
-        /**
-         * Use NotificationCompat.Builder to set up our notification.
-         */
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-
-        /** Set the icon that will appear in the notification bar. This icon also appears
-         * in the lower right hand corner of the notification itself.
-         *
-         * Important note: although you can use any drawable as the small icon, Android
-         * design guidelines state that the icon should be simple and monochrome. Full-color
-         * bitmaps or busy images don't render well on smaller screens and can end up
-         * confusing the user.
-         */
-        builder.setSmallIcon(R.drawable.ic_stat_notification);
-
-        // Set the intent that will fire when the user taps the notification.
-        builder.setContentIntent(pendingIntent);
-
-        // Set the notification to auto-cancel. This means that the notification will disappear
-        // after the user taps it, rather than remaining until it's explicitly dismissed.
-        builder.setAutoCancel(true);
-
-        /**
-         *Build the notification's appearance.
-         * Set the large icon, which appears on the left of the notification. In this
-         * sample we'll set the large icon to be the same as our app icon. The app icon is a
-         * reasonable default if you don't have anything more compelling to use as an icon.
-         */
-        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
-
-        /**
-         * Set the text of the notification. This sample sets the three most commononly used
-         * text areas:
-         * 1. The content title, which appears in large type at the top of the notification
-         * 2. The content text, which appears in smaller text below the title
-         * 3. The subtext, which appears under the text on newer devices. Devices running
-         *    versions of Android prior to 4.2 will ignore this field, so don't use it for
-         *    anything vital!
-         */
-        builder.setContentTitle("BasicNotifications Sample");
-        builder.setContentText("Time to learn about notifications!");
-        builder.setSubText("Tap to view documentation about notifications.");
-
-        // END_INCLUDE (build_notification)
-
-        // BEGIN_INCLUDE(send_notification)
-        /**
-         * Send the notification. This will immediately display the notification icon in the
-         * notification bar.
-         */
         NotificationManager notificationManager = (NotificationManager) getSystemService(
                 NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTIFICATION_ID, builder.build());
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
         // END_INCLUDE(send_notification)
     }
 }
